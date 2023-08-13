1e9456154a62635a06fda320ab190025ea02e36f
Orhun Aka <orhunaka@proton.me>
2023-08-13 08:15:48 +0300

    finished the profile card challenge 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="orhun.jpg" alt="Orhun Aka" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Orhun Aka</h1>
+      <p>Front-end web developer who enjoys any kind of project!</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="😈" color="lime" />
+      <Skill skill="HTML + CSS" emoji="😈" color="orangered" />
+      <Skill skill="Git and GitHub" emoji="😈" color="yellow" />
+      <Skill skill="JavaScript" emoji="😈" color="orange" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
